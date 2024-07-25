13a83f17cb0740cf19c5c24a5634b129ed69fee3
Borivoj Grujicic <borivoj.grujicic@gmail.com>
2024-07-25 10:22:11 +0200

    add nums

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
+    //running example
     let example1 = warp::get()
         .and(warp::path("api"))
         .and(warp::path("HttpTest"))
@@ -14,11 +15,24 @@ async fn main() {
             None => Response::builder().body(String::from("This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.")),
         });
 
+    // End point for adding 2 numbers
+    let add_numbers = warp::get()
+        .and(warp::path("api"))
+        .and(warp::path("AddNumbers"))
+        .and(warp::query::<HashMap<String,String>>())
+        .map(|p: HashMap<String, String>|{
+            let num1 = p.get("num1").and_then(|n|n.parse::<i32>().ok()).unwrap_or(0);
+            let num2 = p.get("num2").and_then(|n| n.parse::<i32>().ok()).unwrap_or(0);
+            let sum = num1 + num2;
+            Response::builder().body(format!("The sum of {} and {} is {}:", num1, num2, sum ))
+        });
+
     let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
     let port: u16 = match env::var(port_key) {
         Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
         Err(_) => 3000,
     };
 
-    warp::serve(example1).run((Ipv4Addr::LOCALHOST, port)).await
+    warp::serve(example1.or(add_numbers)).run((Ipv4Addr::LOCALHOST, port)).await;
 }
+
